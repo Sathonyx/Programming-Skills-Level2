6dc9f01d0270802793f0128d956ed8ebde804fdb
Sathonyx <adrianreyeslim@gmail.com>
2024-01-18 12:55:08 +0100

    Probamos

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/*
+ The lottery system produces results consisting of 4 digits + 1 letter, e.g., 0345F. Develop a lottery ticket purchase system with the following features:
+Users can choose from the following tickets:
+5678B,9876C,2345D,6789E,3456F,8765G,4321H,7890J,5432K,2109L,8765M,1357N,2468P,6543Q,7891R,3579S,9821T,4682U,5763V,1234A
+-Users can buy a minimum of 1 and a maximum of 2 tickets.
+-Payment is accepted in cash, and each ticket costs 1 USD.
+-After choosing tickets and quantity, the system prompts the user to pay in cash or by bank card.
+-This system only accepts 1 USD and 5 USD bills. The user must choose the bill to use for payment, and the system should return the change if applicable.
+-After payment, the ticket is issued.
+-The user returns to the main menu to play the lottery.
+-The lottery system generates 1 random ticket code.
+*/
+public class Main {
+
+    Ticket tickets;
+
+    public static void main(String[] args) {
+
+
+        }
+    }
---
+++ src/Ticket.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Ticket {
+
+    HashMap<Integer,String> ticketOptions= new HashMap<>();
+    String[] ticketPool = {"5678B","9876C","2345D","6789E","3456F","8765G","4321H","7890J","5432K","2109L","8765M","1357N","2468P","6543Q","7891R","3579S","9821T","4682U","5763V","1234A"};
+    int keyValue = 0;
+
+    public Ticket() {
+        for(String ticket: ticketPool){
+            keyValue++;
+            ticketOptions.put(keyValue,ticket);
+
+        }
+
+    }
+}
